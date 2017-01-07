910b47685bc47486e573c642036c579140373f67
Andrew <andrew.carroll1@gmail.com>
2017-01-07 02:14:14 -0500

    gamma

--- main.py
+++ main.py
@@ -33,9 +33,6 @@ class CPUTemp:
 def set_gamma(g):
 	sense.gamma = g
 
-def up_gamma():
-	if sense.gammma
-
 sense = SenseHat();
 
 gamma10 = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 10, 10]
