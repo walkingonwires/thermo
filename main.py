926e711e683acce51164dcc9483f07121a8df5ca
Andrew <andrew.carroll1@gmail.com>
2017-01-07 00:50:47 -0500

    added display code

--- main.py
+++ main.py
@@ -23,12 +23,6 @@ class CPUTemp:
         tempraw = self.read()
         return float(tempraw[:-3] + "." + tempraw[-3:])
 
-    def get_temperature_in_f(self):
-        return self.convert_c_to_f(self.get_temperature_in_c())
-
-    def convert_c_to_f(self, c):
-        return c * 9.0 / 5.0 + 32.0
-
     def __exit__(self, type, value, traceback):
         self.close()
 
@@ -38,12 +32,40 @@ class CPUTemp:
 
 sense = SenseHat();
 sense.low_light = True
-sense.rotation = 180
-
 loop = 1
 
+OFFSET_LEFT = 1
+OFFSET_TOP = 2
+
+NUMS =[1,1,1,1,0,1,1,0,1,1,0,1,1,1,1,  # 0
+       0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,  # 1
+       1,1,1,0,0,1,0,1,0,1,0,0,1,1,1,  # 2
+       1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,  # 3
+       1,0,0,1,0,1,1,1,1,0,0,1,0,0,1,  # 4
+       1,1,1,1,0,0,1,1,1,0,0,1,1,1,1,  # 5
+       1,1,1,1,0,0,1,1,1,1,0,1,1,1,1,  # 6
+       1,1,1,0,0,1,0,1,0,1,0,0,1,0,0,  # 7
+       1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,  # 8
+       1,1,1,1,0,1,1,1,1,0,0,1,0,0,1]  # 9
+
+# Displays a single digit (0-9)
+def show_digit(val, xd, yd, r, g, b):
+  offset = val * 15
+  for p in range(offset, offset + 15):
+    xt = p % 3
+    yt = (p-offset) // 3
+    sense.set_pixel(xt+xd, yt+yd, r*NUMS[p], g*NUMS[p], b*NUMS[p])
+
+# Displays a two-digits positive number (0-99)
+def show_number(val, r, g, b):
+  abs_val = abs(val)
+  tens = abs_val // 10
+  units = abs_val % 10
+  if (abs_val > 9): show_digit(tens, OFFSET_LEFT, OFFSET_TOP, r, g, b)
+  show_digit(units, OFFSET_LEFT+4, OFFSET_TOP, r, g, b)
+
 def toFarenheit(c):
-	return  9.0/5.0 * c + 32
+	return  int(9.0/5.0 * c + 32)
 
 def tempLoop():
 	t = sense.get_temperature()
@@ -52,7 +74,10 @@ def tempLoop():
 	with CPUTemp() as cpu_temp:
 		c = cpu_temp.get_temperature()
 	temp_calc = ((t+p+h)/3) - (c/5)
-	sense.show_message(str(round((toFarenheit(temp_calc)), 1)), scroll_speed=0.2, text_colour=[139,0,0], back_colour=[0,0,0])
+	sense.clear()
+	f_temp = toFarenheit(temp_calc)
+	show_number(int(f_temp), 255, 0 , 190)
+	print f_temp
 	Timer(15.0, tempLoop).start()
 
 tempLoop()
