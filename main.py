fc9ddb2372a7d1a344ecfe3d4687d24c79ef6380
Andrew <andrew.carroll1@gmail.com>
2017-01-06 19:59:06 -0500

    tweaked temp calc

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ sense.rotation = 180
 loop = 1
 
 def tempLoop():
-	temp = sense.get_temperature_from_pressure()
-	sense.show_message(str(round((temp + 32), 2)), scroll_speed=0.2, text_colour=[139,0,0], back_colour=[0,0,0])
+	t = ap.get_temperature()
+	p = ap.get_temperature_from_pressure()
+	h = ap.get_temperature_from_humidity()
+	with CPUTemp() as cpu_temp:
+		c = cpu_temp.get_temperature()
+	temp_calc = ((t+p+h)/3) - (c/5)
+	sense.show_message(str(round((temp_calc + 32), 2)), scroll_speed=0.2, text_colour=[139,0,0], back_colour=[0,0,0])
 	Timer(15.0, tempLoop).start()
-	
+
 tempLoop()
