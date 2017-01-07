823720dec057b316ef0b6061cf13498f964a3f94
Andrew <andrew.carroll1@gmail.com>
2017-01-06 19:54:10 -0500

    test

--- riley.py
+++ riley.py
@@ -9,7 +9,7 @@ sense.rotation = 180
 
 def tempLoop():
 
-	sense.show_message("Riley Carroll", scroll_speed=0.2, text_colour=[139,0,0], back_colour=[0,0,0])
-	Timer(15.0, tempLoop).start()
+	sense.show_message("POOPDICK", scroll_speed=0.2, text_colour=[139,0,0], back_colour=[0,0,0])
+	Timer(8.0, tempLoop).start()
 
 tempLoop()
