72d3a4566aa2501c1b567d2ec2107a9464210fab
Andrew <andrew.carroll1@gmail.com>
2017-02-12 13:46:24 -0500

    debugging

--- app.py
+++ app.py
@@ -117,9 +117,7 @@ def tempLoop():
     sense.clear()
     f_temp = toFarenheit(temp_calc)
     show_number(int(f_temp), 255, 0 , 190)
-    print 'old temp: ' + str(current_temp)
-    current_temp = f_temp
-    print 'new temp: ' + str(current_temp)
+    global current_temp = f_temp
     Timer(15.0, tempLoop).start()
 
 tempLoop()
