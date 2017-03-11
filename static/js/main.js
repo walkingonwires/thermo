551ed5f7b39b66045845c2f875c2afedd60e0ce5
Andrew <andrew.carroll1@gmail.com>
2017-03-11 13:43:06 -0500

    styling

--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
 
     function getCurrentTemp () {
         $.get('/current-temp').then(function (res) {
+            res = JSON.parse(res);
             $curTempView.text(res.currentTemp);
         }).catch(function (err) {
             console.warn("didn't get temp \n" + err);
