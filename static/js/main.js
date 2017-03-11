b9cb1164cc325c077b941d8042db0ee46702afc3
Andrew <andrew.carroll1@gmail.com>
2017-03-11 13:35:54 -0500

    moved js to static asset. starting ui

---
+++ static/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    var $curTempView = $('#currentTemp'),
+        $targTempView = $('#targetTemp'),
+        localTarget = null,
+        officialTarget = null,
+        currentPiTemp = null;
+            
+    
+
+    function getCurrentTemp () {
+        $.get('/current-temp').then(function (res) {
+            $curTempView.text(res.currentTemp);
+        }).catch(function (err) {
+            console.warn("didn't get temp \n" + err);
+        });
+    }
+    
+    function getTargetTemp() {
+        
+    }
+
+    function updateTargetTemp() {
+        
+    }
+    
+    function increaseTargetTemp() {
+        
+    }
+    
+    function decreaseTargetTemp() {
+        
+    }
+    
+    function toggleMode () {
+        
+    }
+
+    getCurrentTemp()
+    
+    
+
+});
